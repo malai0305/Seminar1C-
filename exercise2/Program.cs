dc247a4e253674c75586341553806f51f0a561fc
unknown <artem03051987@gmail.com>
2022-08-17 00:04:11 +0500

    являются ли три числа сторонами треугольника

---
+++ exercise2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить являются ли три числа сторонами треугольника.
+bool CheckTriagle (int a,int b, int c)=> a+b>c&&b+c>a&&a+c>a;
+Console.WriteLine("Введите первое число :");
+int a = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите второе число :");
+int b = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите третье число :");
+int c = int.Parse(Console.ReadLine()??"0");
+bool check=CheckTriagle(a,b,c);
+Console.WriteLine(check?"Три числа являются сторонами треугольника":"Три числа не являются сторонами треугольника ");
