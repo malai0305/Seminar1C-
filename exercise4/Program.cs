fd92e8233bf1a6634b0a671b9db8c2122c3515f2
unknown <artem03051987@gmail.com>
2022-08-17 01:52:41 +0500

    показать числа фибоначчи меньше заданногочисла N

---
+++ exercise4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать числа Фибоначчи меньше заданного числа N.
+int[]fibonacci = new int [1000];
+fibonacci[0]=1;
+fibonacci[1]=1;
+int n = int.Parse(Console.ReadLine()??"0");
+Console.Write($"{fibonacci[0]}{fibonacci[1]}");
+int index = 1;
+while(fibonacci[index]<n)
+{
+    index++;
+    fibonacci[index]=fibonacci[index-1]+fibonacci[index-2];
+    if (fibonacci[index]<n)
+    Console.Write($"{fibonacci[index]}");
+}
