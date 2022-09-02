dc4be4764694447e6f6724664f7c6c828168c1d8
unknown <artem03051987@gmail.com>
2022-09-02 15:35:47 +0500

    Рекурсия,показать натуральные числа от 1 до n

---
+++ exercise6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Рекурсия.Показать натуральные числа от 1 до N, N заданно.
+void number(int m,int n)
+{
+    if(m<=n)
+    {
+        Console.Write("{0}",m);number(m+1,n);
+
+    }
+}
+int n = 5;
+int m = 1;
+number(m,n);
+Console.WriteLine();
